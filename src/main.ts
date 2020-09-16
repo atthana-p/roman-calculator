2e7e889c582aeabd8fbc15b3802cc3a1bd3d33d8
Pathomphong Intajai <pathomphong.i@codium.co>
2020-09-16 13:59:47 +0700

    init fn dec_to_roman

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 
-export function dec_to_roman(number): string {
-    const 
-    if (number < 4 ) {
-        return 'No winner'
+export function dec_to_roman(Dec_of_roman: number): string  {
+    const Roman = ''
+    if (Dec_of_roman < 10) {
+        console.log("GGWP")
     }
+    return 'III'+String(Roman) 
 
 }
