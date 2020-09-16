dfb686bdb851c5240032e6a003272e419fcbdb6e
atthana-p <atthana.p@codium.co>
2020-09-16 14:32:58 +0700

    Initial my test code (advised by my idol-Earch).

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 
-export function dec_to_roman(number): string {
-    const 
-    if (number < 4 ) {
+export function dec_to_roman(num: number): string {
+    if (num < 4 ) {
         return 'No winner'
     }
-
+    return ''
 }
